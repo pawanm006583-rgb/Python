2620cca2b533346a072a1e24540c33c81147c7aa
Pawan Mishra <pawanm006583@gmail.com>
2026-01-26 19:27:03 +0530

    list.py

---
+++ Lists.py
@@ -0,0 +1,22 @@
+Marks=[]
+
+f1 = int(input("Enter your marks:"))
+Marks.append(f1)
+
+f2 = int(input("Enter your marks:"))
+Marks.append(f2)
+
+f3 = int(input("Enter your marks:"))
+Marks.append(f3)
+
+f4 = int(input("Enter your marks:"))
+Marks.append(f4)
+
+f5 = int(input("Enter your marks:"))
+Marks.append(f5)
+
+f6 = int(input("Enter your marks:"))
+Marks.append(f6)
+
+Marks.sort()
+print(Marks)
