ad1174b3e6887c78788b74d5af5e0069690497e5
Pawan Mishra <pawanm006583@gmail.com>
2026-01-27 23:14:26 +0530

    functions

---
+++ Functions.py
@@ -0,0 +1,44 @@
+'''This is how the functions is used and defind '''
+# def say_hello():
+#     print("Hello!")
+# say_hello()
+
+
+# Name = input("Enter your name:")
+# def greet(Name):
+#     print("Hello",Name)
+# greet(Name)
+
+
+# def add(a,b):
+#     return a+b
+
+# result = add(5,5)
+# print(result)
+
+
+# def square(n):
+#     return n*n
+# def cube(n):
+#     return n*square(n)
+# print(cube(2))
+
+
+# def is_even(n):
+#     if n%2==0:
+#         print("The number is even") 
+#     else:
+#      print("The number is odd")  
+# a = int(input("Enter the number :-"))       
+# is_even(a)
+
+
+'''1. Create a function to add two numbers.
+Function should take two numbers and print their sum.'''
+
+
+sum1 = int(input("Enter the number :"))
+sum2 = int(input("Enter the number :"))
+def greet(sum1,sum2):
+    return sum1+sum2    
+greet(print(sum1,sum2))
