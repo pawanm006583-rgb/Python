a6bbfc687d3b41402f29c2c5bfd6a393a1d72814
Pawan Mishra <pawanm006583@gmail.com>
2026-01-28 20:34:23 +0530

    conversation

---
+++ conversation.py
@@ -0,0 +1,28 @@
+# user_name=input("Enter your name: ")
+# print("Hello", user_name)
+
+# a = "sher coder"
+# print(a[5:10:])
+
+# name = "Goast"
+# age = 24
+# print(name , age)
+
+
+# name = "Goast"
+# age = 24
+# print("Hello my name is",name,"and my age is ",age)
+
+# number = input("Enter your number:")
+# # age = input("What is your age:")
+# print(number)
+# print(age)
+
+# a=print(ord("A"))
+# b=print(ord("B"))
+# print("a"<="b")
+
+a = "A" 
+b="H"
+print(ord("A"))
+print(ord("H"))
