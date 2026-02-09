97baa72cec94850462a924cbc8839dec2e35b0c4
Pawan Mishra <pawanm006583@gmail.com>
2026-02-09 20:04:57 +0530

    Marks.py

---
+++ marks.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the marks of first subject"))
+b = int(input("Enter the marks of second subject"))
+c = int(input("Enter the marks of Third subject"))
+d = int(input("Enter the marks of forth subject"))
+e = int(input("Enter the marks of fivth subject"))
+
+print(a+b+c+d)
+print(a+b+c+d/5)
+print(a+b+c+d/500)
